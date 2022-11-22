e1c903b5c6cc4948cae2f10ce26d4431464c5c23
OnurMok <onurmok@gmail.com>
2022-11-22 18:03:36 +0100

    Exercises 19.11.22

---
+++ Exercises/Ex.2/main.js
@@ -0,0 +1,18 @@
+console.log("Test")
+
+let length = 18;
+console.log(length)
+let lastName = "Johnson";
+console.log(lastName)
+
+let a = 10;
+let b = 30;
+let c = a + b;
+
+console.log(c);
+console.log("The sum of " + a + " and " + b + " is: " + (a+b));
+
+let a1 = 1000;
+let b1 = 60;
+let c1 = a1 / b1;
+console.log(Math.round(c1));
