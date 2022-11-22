44bfccd8f1b1b5171853c7a29e764033db45efe2
OnurMok <onurmok@gmail.com>
2022-11-22 20:51:34 +0100

    Work on JS chapter 2

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.2/main.js
@@ -0,0 +1,8 @@
+alert("Test")
+
+
+let name = "Onur";
+let age = "37";
+let cancode = "true";
+
+console.log(`Hello, my name is ${name}, I am ${age} years old and i can code JavaSrcipt: ${cancode}`);
