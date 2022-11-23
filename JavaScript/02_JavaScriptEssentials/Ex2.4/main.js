46f47a000e761c718d38f20dbc4617dc80b4bf37
OnurMok <onurmok@gmail.com>
2022-11-23 23:06:09 +0100

    Exc 2.4 done

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.4/main.js
@@ -0,0 +1,13 @@
+alert("Exercise 2.4")
+
+let a=100;
+console.log(a)
+let b=20;
+console.log(b)
+let c=20;
+console.log(c)
+
+let action = prompt("Do you want to see the action?");
+console.log(a+=b);
+console.log(a/=c);
+console.log(c=c%b);
