0aa19376ad60186a14b15318b6cfef2d9e219d89
OnurMok <onurmok@gmail.com>
2022-11-23 22:23:38 +0100

    Finding Hypotenuse

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.3/main.js
@@ -0,0 +1,14 @@
+alert("test");
+
+let a = prompt("Please enter the value for a");
+console.log(a)
+
+let b = prompt("Please enter the value of b");
+console.log(b)
+
+a = a * a 
+b = b * b
+
+c = a + b 
+c = c*
+console.log(c)
