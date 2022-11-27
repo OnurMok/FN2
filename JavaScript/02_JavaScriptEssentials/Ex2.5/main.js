7170fbebc079daee87692f411964a16688bc0509
OnurMok <onurmok@gmail.com>
2022-11-27 22:07:47 +0100

    Exercise 2.5 from 26.11.22

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.5/main.js
@@ -0,0 +1,14 @@
+alert("Exercise 2.5")
+
+let mile = 1.60934
+console.log(mile, "is one mile");
+
+let distanceKms = 130
+console.log(distanceKms, "is ditance in kms");
+
+let kmsToMiles = mile * distanceKms
+console.log(kmsToMiles, "is 130km in miles");
+
+console.log(`The distance of ${distanceKms} kms is equal to ${kmsToMiles} miles.`)
+
+
