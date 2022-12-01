eb688db6f6c17ee85195bb9fe4ba00ae42960ce5
OnurMok <onurmok@gmail.com>
2022-12-01 20:45:11 +0100

    Project1 from 1.12.2022

---
+++ JavaScript/03_JavaScriptMultipleValues/Project1/main.js
@@ -0,0 +1,32 @@
+alert("Project1");
+
+const theList = ['Laurence', 'Svekis', true, 35, null, undefined, {test: 'one', score: 55}, ['one', 'two']]
+
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.shift();
+console.log(theList);
+
+theList.unshift("First");
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.pop();
+console.log(theList);
+
+theList.push("Middle", "Hello world", "Last");
+console.log(theList);
