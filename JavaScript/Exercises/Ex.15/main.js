ea2cebcc9a4a702a2d148c295ed23b8436367537
OnurMok <onurmok@gmail.com>
2022-12-03 19:21:28 +0100

    Ex.15 from 3.12.2022

---
+++ JavaScript/Exercises/Ex.15/main.js
@@ -0,0 +1,10 @@
+alert("Ex.15");
+
+//Write a JavaScript program to push an item in array of strings;
+
+
+let program = ["Day","Night","Morning"];
+console.log(program);
+
+program.push("afternoon","night");
+console.log(program);
