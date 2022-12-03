6ecec09590c46c2304bb5d76017c52db5dbd5e8d
OnurMok <onurmok@gmail.com>
2022-12-03 18:46:56 +0100

    Ex.8 from 3.12.2022

---
+++ JavaScript/Exercises/Ex.8/main.js
@@ -0,0 +1,9 @@
+alert("Ex.8");
+
+//Write a program which will take from 3th element until the end of an array;
+
+let program = ["Blue","Yellow","White","Black","Purple","Green"];
+console.log(program);
+
+let program2 = program.slice(3,5);
+console.log(program2);
