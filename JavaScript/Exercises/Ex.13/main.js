9193f4c9bcb5350ba3b4b3660736da32c95bff05
OnurMok <onurmok@gmail.com>
2022-12-03 19:15:57 +0100

    Ex.13 from 3.13.2022

---
+++ JavaScript/Exercises/Ex.13/main.js
@@ -0,0 +1,9 @@
+alert("Ex.13");
+
+// Write a JavaScript program to find min in an array of integers;
+
+let program = [3,5,7,8,9];
+console.log(program);
+
+let minnumbers = Math.min.apply(Math, program);
+console.log(minnumbers);
