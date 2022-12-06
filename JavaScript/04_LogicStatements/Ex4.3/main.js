dc1dde4215599c99521de8b4be0d846988bf666e
OnurMok <onurmok@gmail.com>
2022-12-06 19:49:30 +0100

    Ex4.3 edited from 6.12.2022

---
+++ JavaScript/04_LogicStatements/Ex4.3/main.js
@@ -0,0 +1,14 @@
+alert("Ex4.3");
+
+let ID = true; 
+
+let message = ID ? "Yes its true" : "Not is not true";
+
+console.log(message);
+
+ID = true; 
+
+message = !ID ? "Yes its true" : "Not is not true";
+
+console.log(message);
+
