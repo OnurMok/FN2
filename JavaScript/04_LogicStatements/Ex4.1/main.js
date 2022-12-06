4cbbbaf9bb97ea8f54d4cc74ad488b6633eb5cdb
OnurMok <onurmok@gmail.com>
2022-12-06 18:39:22 +0100

    Ex4.1 from 6.12.22

---
+++ JavaScript/04_LogicStatements/Ex4.1/main.js
@@ -0,0 +1,31 @@
+alert("Ex4.1 from 6.12.2022")
+
+let Onur = true; 
+console.log(Onur);
+
+if(Onur){
+    console.log("Yes Onur is true variable");
+} else {
+    console.log("No Onur is not true variable");
+}
+
+if(!Onur){
+    console.log("Yes Onur is true variable");
+} else {
+    console.log("No Onur is not true variable");
+}
+
+Onur = false; 
+console.log(Onur);
+
+if(Onur){
+    console.log("Yes Onur is true variable");
+} else {
+    console.log("No Onur is not true variable");
+}
+
+if(!Onur){
+    console.log("Yes Onur is true variable");
+} else {
+    console.log("No Onur is not true variable");
+}
