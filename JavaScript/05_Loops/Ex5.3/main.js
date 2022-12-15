0af1aeada4105d092d0e758974d15befc7e73240
OnurMok <onurmok@gmail.com>
2022-12-15 19:10:33 +0100

    Ex5.3

---
+++ JavaScript/05_Loops/Ex5.3/main.js
@@ -0,0 +1,13 @@
+
+let myWork = [];
+
+for (let i = 1; i < 11; i++) {
+    myWork.push(
+        {
+            name: "Lesson " + i,
+            status: i % 2 > 0 ? true : false 
+    }
+    );
+}
+
+console.log(myWork);
