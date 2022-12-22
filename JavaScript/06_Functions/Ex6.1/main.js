62b9317a1469a979a60ac1d78a2ed232db1f9537
OnurMok <onurmok@gmail.com>
2022-12-22 19:16:28 +0100

    Ex6.1

---
+++ JavaScript/06_Functions/Ex6.1/main.js
@@ -0,0 +1,10 @@
+alert("Ex6.1");
+
+function nameOfTheFunction(param1,param2) {
+    return param1+param2;
+}
+let num1= 3
+let num2= 5
+
+let result = nameOfTheFunction(num1,num2);
+console.log(result);
