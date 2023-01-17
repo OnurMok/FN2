ae92d6e88f525f0d38f1fcf5ed2f30bf8262ea0c
OnurMok <onurmok@gmail.com>
2023-01-17 19:08:41 +0100

    w3.Ex1.funtions

--- JavaScript/Exercises/w3Resource/Functions/Ex.1/main.js
+++ JavaScript/Exercises/w3Resource/Functions/Ex.1/main.js
@@ -5,14 +5,26 @@ Expected Output : 34223
 */
 
 
-let x = 32243;
-let stringOfX= x.toString();
-let arreyOfX = stringOfX.split("");
+// let x = 32243;
+// let stringOfX= x.toString();
+// let arreyOfX = stringOfX.split("");
+// console.log(arreyOfX);
+// let reverseArrofX = arreyOfX.reverse();
+// console.log(reverseArrofX);
+// let reverseStringofX = reverseArrofX.join("");
+// console.log(reverseStringofX);
+// let reverseX = parseInt(reverseStringofX);
+// console.log(reverseX);
 
-console.log(arreyOfX);
-let reverseArrofX = arreyOfX.reverse();
-console.log(reverseArrofX);
-let reverseStringofX = reverseArrofX.join("");
-console.log(reverseStringofX);
-let reverseX = parseInt(reverseStringofX);
-console.log(reverseX);
+
+
+function ReversNumber(x) {
+    let stringOfX= x.toString();
+    let arreyOfX = stringOfX.split("");
+    let reverseArrofX = arreyOfX.reverse();
+    let reverseStringofX = reverseArrofX.join("");
+    return parseInt(reverseStringofX);
+}
+console.log(ReversNumber(322423));
+console.log(ReversNumber(342534234245));
+console.log(ReversNumber(44455545));
