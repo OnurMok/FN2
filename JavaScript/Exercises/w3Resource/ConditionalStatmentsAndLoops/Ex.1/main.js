93f7d3f91609da180d2fb0d18cad77d4a9d3a28d
OnurMok <onurmok@gmail.com>
2023-01-17 19:43:12 +0100

    w3.ex1. con&loops

---
+++ JavaScript/Exercises/w3Resource/ConditionalStatmentsAndLoops/Ex.1/main.js
@@ -0,0 +1,16 @@
+/*
+1. Write a JavaScript program that accept two integers and display the larger.
+*/
+
+let num1 = 51;
+let num2 = 13;
+
+if (num1 > num2) {
+    console.log(num1 + " is larger than " + num2);
+    
+} else if (num2 > num1) {
+    console.log(num1 + " is larger than " + num1);
+
+} else {
+    console.log(" The numbers are equel");
+}
