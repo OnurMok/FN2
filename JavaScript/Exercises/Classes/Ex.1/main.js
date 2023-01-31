3c9a9554e2af41022f8ab2742cc0f0e72485e658
OnurMok <onurmok@gmail.com>
2023-01-31 20:38:28 +0100

    a

---
+++ JavaScript/Exercises/Classes/Ex.1/main.js
@@ -0,0 +1,14 @@
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    area() {
+        console.log(`The area of rectangle is ${this.width * this.height}`);
+    }
+}
+
+let Recatngle1 = new Rectangle(7,2);
+
